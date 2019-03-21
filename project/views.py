5837e6d84ff225a359c5981cf0060637466df388
thewolfe1 <angrybeats@gmail.com>
2019-03-21 17:51:59 +0200

    About page

--- project/views.py
+++ project/views.py
@@ -34,6 +34,21 @@ def post_new(request):
         form = PostForm()
     return render(request, 'project/post_edit.html', {'form': form})
 
+def about(request):
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(subject, message, from_email, ['admin@example.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect('success')
+    return render(request, 'blog/about.html', {'form': form})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
