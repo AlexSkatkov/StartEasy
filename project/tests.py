2a78cf9324d1bfe5fcb5f7df18d9b130a42274d6
Olga <olg200492@gmail.com>
2019-03-21 22:34:33 +0200

    unit test view post_new

--- project/tests.py
+++ project/tests.py
@@ -1,14 +1,17 @@
 from django.test import TestCase,Client
 from django.urls import reverse
-from StartEasy import project
+from StartEasy.project import views,urls
 
-import json
 
 class Test_Views(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.list_url = reverse('list')
+
     def test_views_home(self):
-        client = Client()
-
-        response = client.get(reverse('list'))
-
+        response = self.client.get(self.list_url)
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'project/home.html')
+
+     def test_views_post_new_POST(self):
