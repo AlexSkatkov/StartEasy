50a75105caf4f0a7d1d980f4c35d22a205c4199a
Olga <olg200492@gmail.com>
2019-03-21 21:15:36 +0200

    unit test Views

--- project/tests.py
+++ project/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase,Client
+from django.urls import reverse
+from StartEasy import project
 
-# Create your tests here.
+import json
+
+class Test_Views(TestCase):
+    def test_views_home(self):
+        client = Client()
+
+        response = client.get(reverse('list'))
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'project/home.html')
