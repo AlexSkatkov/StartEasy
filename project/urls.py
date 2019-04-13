ac7c97f96c61b72b02f01b315a8cc98f7b3e1e76
ofekdemri <ofekdemri27@gmail.com>
2019-04-13 13:11:19 +0300

    help update
    ofek demri

--- project/urls.py
+++ project/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('post/<int:pk>/delete/', views.post_delete, name='post_delete'),
-    path('help/', views.help, name='project-help'),
+    path('help/', views.help, name='help'),
     path('about/', views.about, name='project-about')
 
 ]
