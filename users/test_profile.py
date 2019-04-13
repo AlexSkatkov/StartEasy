4a745621b3ef698b56253467e14893bcba1cb5e8
ofekdemri <ofekdemri27@gmail.com>
2019-04-13 15:15:00 +0300

    profile pic unit test
    ofek demri

--- users/test_profile.py
+++ users/test_profile.py
@@ -3,14 +3,9 @@ from .forms import ProfileUpdateForm,UserUpdateForm
 from django.test import Client
 
 class TestProfile(TestCase):
-    def test_form1(self):
+    def test_form(self):
         data = {'image': 'Foo'}
         form = ProfileUpdateForm(data=data)
-        self.assertTrue(form.is_valid())
-
-    def test_form2(self):
-        data = {'email': 'Foo@gmail.com'}
-        form = UserUpdateForm(data=data)
         self.assertTrue(form.is_valid())
 
     def test_post_404(self):
