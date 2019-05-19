1adbba7cc0b3c69b8fb1d7affa84f60ca51f720d
Olga <olg200492@gmail.com>
2019-05-19 13:48:47 +0300

    olga lapovsky

--- setup.py
+++ setup.py
@@ -2,3 +2,9 @@ import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-your-username",
+    version="0.0.1",
+    author="Example Author",
+    author_email="author@example.com",
